0fb51d814eacb24e78a641cd9674579dda216258
Ace Of Snakes <aceofsnakesmain@gmail.com>
2024-06-02 21:40:04 +0300

    add debian packaging

---
+++ distr/debian.sh
@@ -0,0 +1,14 @@
+TARGET=/tmp/xxx
+SOURCE=./
+mkdir -p $TARGET
+rm -rf $TARGET
+mkdir -p $TARGET/debian
+cp ${SOURCE}/debian/ -r $TARGET/
+cp -r ../src/ $TARGET/src/
+cp -r ../settings/ $TARGET/settings/
+cp  ../* $TARGET/
+cd $TARGET
+ln -s /usr/bin/qmake6 ./qmake
+pwd
+
+dpkg-buildpackage -b -uc -us
