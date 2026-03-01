3b9badfba3940a1c0e6755baa26944d21e4eb1c4
Ace Of Snakes <AceOfSnakes@gmail.com>
2026-03-01 12:19:56 +0300

    change version

--- distr/debian.sh
+++ distr/debian.sh
@@ -9,5 +9,5 @@ cd $PWDFROM
 echo "==============================================================="
 echo $0 PWD: $(pwd)
 echo "==============================================================="
-
+export APP_VERSION_VALUE=26.03
 dpkg-buildpackage -B -uc -us --hook-done='for f in ../*amd64.deb; do mv "$f" "${f%%_amd64.deb}_$(uname -m).deb"; done'
