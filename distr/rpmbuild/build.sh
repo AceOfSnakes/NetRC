2e546bba583657c1dcb6d1f53f3d366e3c80ebfe
Ace Of Snakes <AceOfSnakes@gmail.com>
2026-04-26 13:18:44 +0300

    build RPM in /ymp

--- distr/rpmbuild/build.sh
+++ distr/rpmbuild/build.sh
@@ -1,23 +1,32 @@
 #!/bin/bash -e
 
 CURRENT_DIR=$(pwd)
-RPM_ROOT="$CURRENT_DIR/rpm"
 
-rm -rf $RPM_ROOT
+cd ../..
+NAME=$(basename "$PWD")
+DIR=$PWD
+RPM_ROOT=/tmp/$NAME/rpm
 
+cd $CURRENT_DIR
 # 1. 
+echo ========================= $RPM_ROOT
+rm -rf $RPM_ROOT
 mkdir -p "$RPM_ROOT"/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 
 # 2. 
-cp netrc.spec "$RPM_ROOT/SPECS/"
 
-mkdir -p $RPM_ROOT/SOURCES
+cp netrc.spec "$RPM_ROOT/.."
+
 cp -r ../../src $RPM_ROOT/SOURCES
 cp -r ../../*.pro $RPM_ROOT/SOURCES
 cp -r ../../settings $RPM_ROOT/SOURCES
 cp -r ../../src/commons/style $RPM_ROOT/SOURCES
 cp -r ../debian/*.desktop $RPM_ROOT/SOURCES
 export APP_VERSION_VALUE=26.04
+
+
+cd "$RPM_ROOT/.."
+
 # 3. 
 rpmbuild -ba \
   --define "_topdir $RPM_ROOT" \
@@ -26,3 +35,4 @@ rpmbuild -ba \
 
 echo "========================================"
 echo "Complete. Find your packages in: $RPM_ROOT/RPMS/"
+cp $RPM_ROOT/RPMS/*.rpm $RPM_ROOT/../../
